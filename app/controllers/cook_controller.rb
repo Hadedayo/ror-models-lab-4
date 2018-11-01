2c87e8eaa0e780dd3d93183522d7040dcaa1096f
Hassanat Adedayo <hadedayo@spartaglobal.com>
2018-11-01 17:21:37 +0000

    Deleted the @title for both vhef and dish

--- app/controllers/cook_controller.rb
+++ app/controllers/cook_controller.rb
@@ -1,12 +1,10 @@
 class CookController < ApplicationController
   def chef
-    @title = "Chef information"
     @all_my_chefs = Chef.all
   end
 
   def dish
-    @title = "Dish information"
-    # @all_my_dishes = Dish.all
+    @all_my_dishes = Dish.all
   end
 
 
